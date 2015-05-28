6068adf6c99c8c1fda24ecaaac9ea2152469543f
José Cardona Figueroa <cardona5d@gmail.com>
2015-05-28 10:19:03 -0500

    Cambios en los ajustes del proyecto para que se conecte a MySQL

--- Banco/settings.py
+++ Banco/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'retiro',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'Banco.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'banco',
+        'USER': 'root',
+        'PASSWORD': 'LeeesdhdA',
     }
 }
 
