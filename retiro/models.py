3f371960ec276568e8d1445a722d5666069d8590
José Cardona Figueroa <cardona5d@gmail.com>
2015-06-01 11:25:48 -0500

    Se añadieron modelos para las tablas

--- retiro/models.py
+++ retiro/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Retiros(models.Model):
-    user = models.OneToOneField(User, unique=True)
-    cantidad_total = models.FloatField()
+    user = models.OneToOneField(User)
+    monto_retiro = models.FloatField()
     fecha_retiro = models.DateTimeField(auto_now_add=True)
-    
+
+class Cuenta(models.Model):
+    user = models.OneToOneField(User)
+    cantidad_total = models.FloatField()
