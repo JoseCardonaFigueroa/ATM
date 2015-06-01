36d12bb5c3913a0f31199d32e6069b6c7947561d
José Cardona Figueroa <cardona5d@gmail.com>
2015-06-01 16:40:11 -0500

    Generación de template base

--- retiro/views.py
+++ retiro/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
 # Create your views here.
+
 @login_required
 def hola(request):
-
+    
     return render_to_response('retiro/index.html',
                                 context_instance=RequestContext(request, {}))
