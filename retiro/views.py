044b9cf9741a9e12a066953f9c65b14c0413eff2
José Cardona Figueroa <cardona5d@gmail.com>
2015-06-08 10:07:33 -0500

    Validación a en el retiro de dinero para que no se retire más dinero del disponible

--- retiro/views.py
+++ retiro/views.py
@@ -27,14 +27,21 @@ def confirmar(request):
         cuenta = Cuenta.objects.get(user=request.user)
         monto = float(request.GET.get('monto', ''))
         print monto
-        cuenta.cantidad_total = cuenta.cantidad_total - monto
-        cuenta.save()
-        print cuenta.cantidad_total
-        return render_to_response('retiro/confirmar.html',
-                                    context_instance=RequestContext(request,
-                                    {'cantidad_total': cuenta.cantidad_total,
-                                    'monto': monto
-                                    }))
+        if cuenta.cantidad_total > monto:
+            cuenta.cantidad_total = cuenta.cantidad_total - monto
+            cuenta.save()
+
+            return render_to_response('retiro/confirmar.html',
+                                        context_instance=RequestContext(request,
+                                        {'cantidad_total': cuenta.cantidad_total,
+                                        'monto': monto
+                                        }))
+        else:
+            return HttpResponse('No se pudo realizar el retiro \
+             El monto a retirar es mayor al dinero disponible \
+             Monto: %s \
+             Cantidad disponible: %s' % (monto,cuenta.cantidad_total))
+
     else:
         return redirect('/retiro/')
 
